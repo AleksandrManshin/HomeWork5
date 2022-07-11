16e43cc1dd79e2447a00a9d54e8671f8e225a28d
Aleksandr <maivan007@yandex.ru>
2022-07-11 18:31:40 +0400

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+}
+
+int SumNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+        else sum = sum + 0;
+    }
+    return sum;
+}
+
+int[] arr = new int[4];
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine("Сумма чисел нечетных индексов равна : " + SumNumbers(arr));
